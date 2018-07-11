b96f27a6f64b7547c9d62603df66f68c6c4c5ea9
patricioeyjo <40763670+patricioeyjo@users.noreply.github.com>
2018-07-11 10:35:32 -0300

    Add files via upload

---
+++ jsInstruccionIF (1).js
@@ -0,0 +1,17 @@
+function mostrar()
+{
+//tomo la edad  
+
+var edad;
+
+edad = document.getElementById("edad").value;
+
+edad = parseInt(edad);
+
+if (edad == 15)
+{
+	alert("niña bonita")
+
+}
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (3).js
@@ -0,0 +1,20 @@
+function mostrar()
+{
+//tomo la edad  
+var edad;
+
+edad = document.getElementById("edad").value;
+
+edad = parseInt(edad);
+
+if (edad >= 18)
+{
+	alert("es mayor de edad")
+
+}
+else
+{
+	alert("es menor de edad")
+}
+
+}//FIN DE LA FUNCIÓN
