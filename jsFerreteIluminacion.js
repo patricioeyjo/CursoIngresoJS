449e91ba73cc97a1effbf459bc03e211a982d722
patricioeyjo <40763670+patricioeyjo@users.noreply.github.com>
2018-07-23 12:07:23 -0300

    Add files via upload

---
+++ jsFerreteIluminacion.js
@@ -0,0 +1,47 @@
+/*4.	Para el departamento de iluminación:
+Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
+A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
+B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+ ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+
+ */
+function CalcularPrecio () 
+{
+ 	var Cantidad;
+ 	var Marca;
+ 	var precioLampara;
+ 	var precioBruto;
+ 	var descuento;
+ 	
+ 	descuento=1
+ 	precioLampara=35;
+ 	Cantidad=document.getElementById('Cantidad').value;
+ 	Cantidad=parseInt(cantidad);
+ 	Marca=document.getElementById('Marca').value;
+ 	precioBruto=precioLampara*Cantidad 
+
+ 	switch(Cantidad)
+ 	{
+ 		case 1:
+ 		case 2:
+ 			break;
+ 			case 3:
+ 			switch (Marca)
+ 			{ case "ArgentinaLuz":
+ 				break;
+ 					descuento 0,85;
+ 				case "FelipeLamparas":
+ 					descuento 0,75;
+ 				break;
+ 				default
+
+ 			}
+
+ 	}
+ 	precioDescuento=precioBruto*descuento
+ 	document.getElementById('precioDescuento').value=PrecioConDescuento
+
+}
